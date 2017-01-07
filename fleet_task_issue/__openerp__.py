12e6112b6a75873e2a4f54044bd9db563bf1dd7e
stellaf <stella.fredo@gmail.com>
2017-01-07 09:51:41 +0100

    Add files via upload

---
+++ fleet_task_issue/__openerp__.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Addon by stella.fredo@gmail.com based on 
+#    fleet_analytic_account app from CLEARCORP S.A. (<http://clearcorp.co.cr>).
+#    AURIUM TCHNOLOGIES (<http://auriumtechnologies.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name" : 'Fleet Task Issue',
+    "version" : '9.0',
+    "author" : 'stella.fredo@gmail.com',
+    'complexity': 'normal',
+    "description":  """
+Fleet - Task Issues
+=============================
+This module link the fleet and project with one analytic account. 
+when you create one vehicle
+-------------
+		* one project with the same vehicle name will be created under the project menu.
+		* thus you can schedule the task and issues for this vehicle.
+		* smart buttons to this vehicles task and issues will be added to each vehicle's form.
+    """,
+    "category": 'Managing vehicles tasks and issues',
+    "sequence": 3,
+    "website" : "https://github.com/stellaf/odoofleet#9.0",
+    "images" : [],
+    "depends" : [
+                 'fleet',
+				'account',
+				'analytic',
+				'project',
+                 'project_issue'
+                 ],
+    "data" : ['fleet_task_issue_view.xml'],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : [],
+    "test" : [],
+    "auto_install": False,
+    "application": False,
+    "installable": True,
+    'license': 'AGPL-3',
+}
